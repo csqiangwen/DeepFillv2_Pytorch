982d5337d07a0dbd8fb6f2fe36f5741643f0de20
Qiang Wen <csqiangwen@gmail.com>
2020-08-21 18:26:25 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,196 @@
+import os
+import numpy as np
+import cv2
+import torch
+import torch.nn as nn
+import torchvision as tv
+
+import network
+
+# ----------------------------------------
+#                 Network
+# ----------------------------------------
+def create_generator(opt):
+    # Initialize the networks
+    generator = network.GatedGenerator(opt)
+    print('Generator is created!')
+    network.weights_init(generator, init_type = opt.init_type, init_gain = opt.init_gain)
+    print('Initialize generator with %s type' % opt.init_type)
+    return generator
+
+def create_discriminator(opt):
+    # Initialize the networks
+    discriminator = network.PatchDiscriminator(opt)
+    print('Discriminator is created!')
+    network.weights_init(discriminator, init_type = opt.init_type, init_gain = opt.init_gain)
+    print('Initialize discriminator with %s type' % opt.init_type)
+    return discriminator
+
+def create_perceptualnet():
+    # Get the first 15 layers of vgg16, which is conv3_3
+    perceptualnet = network.PerceptualNet()
+    print('Perceptual network is created!')
+    return perceptualnet
+    
+# ----------------------------------------
+#             PATH processing
+# ----------------------------------------
+def text_readlines(filename):
+    # Try to read a txt file and return a list.Return [] if there was a mistake.
+    try:
+        file = open(filename, 'r')
+    except IOError:
+        error = []
+        return error
+    content = file.readlines()
+    # This for loop deletes the EOF (like \n)
+    for i in range(len(content)):
+        content[i] = content[i][:len(content[i])-1]
+    file.close()
+    return content
+
+def savetxt(name, loss_log):
+    np_loss_log = np.array(loss_log)
+    np.savetxt(name, np_loss_log)
+
+def get_files(path):
+    # read a folder, return the complete path
+    ret = []
+    for root, dirs, files in os.walk(path):
+        for filespath in files:
+            ret.append(os.path.join(root, filespath))
+    return ret
+
+def get_names(path):
+    # read a folder, return the image name
+    ret = []
+    for root, dirs, files in os.walk(path):
+        for filespath in files:
+            ret.append(filespath)
+    return ret
+
+def text_save(content, filename, mode = 'a'):
+    # save a list to a txt
+    # Try to save a list variable in txt file.
+    file = open(filename, mode)
+    for i in range(len(content)):
+        file.write(str(content[i]) + '\n')
+    file.close()
+
+def check_path(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    
+# ----------------------------------------
+#    Validation and Sample at training
+# ----------------------------------------
+def save_sample_png(sample_folder, sample_name, img_list, name_list, pixel_max_cnt = 255):
+    # Save image one-by-one
+    for i in range(len(img_list)):
+        img = img_list[i]
+        # Recover normalization: * 255 because last layer is sigmoid activated
+        img = img * 255
+        # Process img_copy and do not destroy the data of img
+        img_copy = img.clone().data.permute(0, 2, 3, 1)[0, :, :, :].cpu().numpy()
+        img_copy = np.clip(img_copy, 0, pixel_max_cnt)
+        img_copy = img_copy.astype(np.uint8)
+        img_copy = cv2.cvtColor(img_copy, cv2.COLOR_RGB2BGR)
+        # Save to certain path
+        save_img_name = sample_name + '_' + name_list[i] + '.jpg'
+        save_img_path = os.path.join(sample_folder, save_img_name)
+        cv2.imwrite(save_img_path, img_copy)
+
+def psnr(pred, target, pixel_max_cnt = 255):
+    mse = torch.mul(target - pred, target - pred)
+    rmse_avg = (torch.mean(mse).item()) ** 0.5
+    p = 20 * np.log10(pixel_max_cnt / rmse_avg)
+    return p
+
+def grey_psnr(pred, target, pixel_max_cnt = 255):
+    pred = torch.sum(pred, dim = 0)
+    target = torch.sum(target, dim = 0)
+    mse = torch.mul(target - pred, target - pred)
+    rmse_avg = (torch.mean(mse).item()) ** 0.5
+    p = 20 * np.log10(pixel_max_cnt * 3 / rmse_avg)
+    return p
+
+def ssim(pred, target):
+    pred = pred.clone().data.permute(0, 2, 3, 1).cpu().numpy()
+    target = target.clone().data.permute(0, 2, 3, 1).cpu().numpy()
+    target = target[0]
+    pred = pred[0]
+    ssim = skimage.measure.compare_ssim(target, pred, multichannel = True)
+    return ssim
+
+## for contextual attention
+
+def extract_image_patches(images, ksizes, strides, rates, padding='same'):
+    """
+    Extract patches from images and put them in the C output dimension.
+    :param padding:
+    :param images: [batch, channels, in_rows, in_cols]. A 4-D Tensor with shape
+    :param ksizes: [ksize_rows, ksize_cols]. The size of the sliding window for
+     each dimension of images
+    :param strides: [stride_rows, stride_cols]
+    :param rates: [dilation_rows, dilation_cols]
+    :return: A Tensor
+    """
+    assert len(images.size()) == 4
+    assert padding in ['same', 'valid']
+    batch_size, channel, height, width = images.size()
+
+    if padding == 'same':
+        images = same_padding(images, ksizes, strides, rates)
+    elif padding == 'valid':
+        pass
+    else:
+        raise NotImplementedError('Unsupported padding type: {}.\
+                Only "same" or "valid" are supported.'.format(padding))
+
+    unfold = torch.nn.Unfold(kernel_size=ksizes,
+                             dilation=rates,
+                             padding=0,
+                             stride=strides)
+    patches = unfold(images)
+    return patches  # [N, C*k*k, L], L is the total number of such blocks
+
+def same_padding(images, ksizes, strides, rates):
+    assert len(images.size()) == 4
+    batch_size, channel, rows, cols = images.size()
+    out_rows = (rows + strides[0] - 1) // strides[0]
+    out_cols = (cols + strides[1] - 1) // strides[1]
+    effective_k_row = (ksizes[0] - 1) * rates[0] + 1
+    effective_k_col = (ksizes[1] - 1) * rates[1] + 1
+    padding_rows = max(0, (out_rows-1)*strides[0]+effective_k_row-rows)
+    padding_cols = max(0, (out_cols-1)*strides[1]+effective_k_col-cols)
+    # Pad the input
+    padding_top = int(padding_rows / 2.)
+    padding_left = int(padding_cols / 2.)
+    padding_bottom = padding_rows - padding_top
+    padding_right = padding_cols - padding_left
+    paddings = (padding_left, padding_right, padding_top, padding_bottom)
+    images = torch.nn.ZeroPad2d(paddings)(images)
+    return images
+
+def reduce_mean(x, axis=None, keepdim=False):
+    if not axis:
+        axis = range(len(x.shape))
+    for i in sorted(axis, reverse=True):
+        x = torch.mean(x, dim=i, keepdim=keepdim)
+    return x
+
+
+def reduce_std(x, axis=None, keepdim=False):
+    if not axis:
+        axis = range(len(x.shape))
+    for i in sorted(axis, reverse=True):
+        x = torch.std(x, dim=i, keepdim=keepdim)
+    return x
+
+
+def reduce_sum(x, axis=None, keepdim=False):
+    if not axis:
+        axis = range(len(x.shape))
+    for i in sorted(axis, reverse=True):
+        x = torch.sum(x, dim=i, keepdim=keepdim)
+    return x
