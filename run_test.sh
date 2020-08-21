750a55904227eab276d730e571fff28201286979
Qiang Wen <csqiangwen@gmail.com>
2020-08-21 18:22:46 +0800

    Create run_test.sh

---
+++ run_test.sh
@@ -0,0 +1,12 @@
+python3 test.py \
+--baseroot './test_data/' \
+--baseroot_mask './test_data_mask/' \
+--results_path './results' \
+--gan_type 'WGAN' \
+--gpu_ids '1' \
+--epoch 40 \
+--batch_size 1 \
+--num_workers 8 \
+--pad_type 'zero' \
+--activation 'elu' \
+--norm 'none' \
